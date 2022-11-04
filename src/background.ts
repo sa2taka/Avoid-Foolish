f94cfaacb572ac9fe1b852647c5d8ae9c2f1564c
sa2taka <sa2taka@gmail.com>
2022-11-05 01:18:05 +0900

    :bug: fix to behavior

--- src/background.ts
+++ src/background.ts
@@ -1,31 +1,11 @@
-import { fetchConfigList } from "./helpers/config-list-operation";
-
-const load = () => {
-  console.log("[avoid-foolish]background load");
-  // @ts-ignore because unregisterContentScripts is not in @types/chrome
-  chrome.scripting.unregisterContentScripts().then(() => {
-    fetchConfigList().then((configList) => {
-      const urls = configList.map((config) => `*://${config.url}*`);
-
-      if (urls.length === 0) {
-        return;
-      }
-
-      // @ts-ignore because registerContentScripts is not in @types/chrome
-      chrome.scripting.registerContentScripts([
-        { id: "hide-target", matches: ["http://*/*", "https://*/*"], js: ["js/hide-target.js"], css: ["css/hide-target.css"] },
-      ]);
-    });
-  });
-};
-
-load();
-chrome.storage.onChanged.addListener(load);
-
-chrome.tabs.onUpdated.addListener(function (tabId, changeInfo) {
+chrome.tabs.onUpdated.addListener(async (tabId, changeInfo) => {
   if (changeInfo.status === "complete") {
-    chrome.tabs.sendMessage(tabId, {
-      message: "TabUpdated",
-    });
+    await chrome.tabs
+      .sendMessage(tabId, {
+        message: "TabUpdated",
+      })
+      .catch(() => {
+        /** ignore */
+      });
   }
 });
